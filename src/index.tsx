ae4109b16e0662750bfbc0d80fa2997f6a6ca2ea
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-01-13 19:54:07 +0000

    Add Apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from "@apollo/client"
+import "./index.css"
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/v1/graphql",
+  cache: new InMemoryCache()
+})
+
+client
+  .query({
+    query: gql`
+      query MyQuery {
+        film {
+          title
+          release_year
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 )
